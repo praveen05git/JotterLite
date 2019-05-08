e8f9f91d92d9b6672cb2d5a607f7cde2514ab02a
PRAVEEN KUMAR <praveen051997@gmail.com>
2019-05-08 23:05:04 +0530

    Tried separate text view for list view but didn't work

--- app/src/main/java/com/example/jotter/fileView.java
+++ app/src/main/java/com/example/jotter/fileView.java
@@ -7,7 +7,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
